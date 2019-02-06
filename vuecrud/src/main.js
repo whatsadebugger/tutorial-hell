b812b8d0a9d00f28ef76524c56b2733c356174ee
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2019-02-06 07:52:27 -0600

    add routes for each component

--- vuecrud/src/main.js
+++ vuecrud/src/main.js
@@ -1,8 +1,39 @@
+import VueRouter from 'vue-router'
+import VueAxios from 'vue-axios'
+
+import Create from './components/Create.vue'
+import Edit from './components/Edit.vue'
+import Index from './components/Index.vue'
+
 import Vue from 'vue'
 import App from './App.vue'
+
+Vue.Use(VueRouter)
+Vue.Use(VueAxios)
+
+const routes = [
+  {
+    name: 'Create',
+    path: '/create',
+    component: Create
+  },
+  {
+    name: 'Edit',
+    path: '/edit',
+    component: Edit
+  },
+  {
+    name: 'Index',
+    path: '/index',
+    component: Index
+  }
+]
+
+const router = new VueRouter({ mode: 'history', routes: routes })
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
