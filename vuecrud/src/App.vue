e920a2e25aeddb8b3c27838483774550d8253a50
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2019-03-18 08:14:43 -0500

    add table classes

--- vuecrud/src/App.vue
+++ vuecrud/src/App.vue
@@ -1,14 +1,34 @@
 <template>
   <div id="app" class="container">
-    <nav class="navbar is-light">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <router-link :to="{ name: 'Create' }" class="nav-link">Add Items</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link :to="{ name: 'Index' }" class="nav-link">All Items</router-link>
-        </li>
-      </ul>
+    <!-- <router-link :to="{ name: 'Create' }" class="nav-link">Add Items</router-link> -->
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <router-link :to="{ name: 'Index' }" class="navbar-item">Devices</router-link>
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-light">Log in</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
 
     <router-view></router-view>
@@ -20,15 +40,15 @@
 export default {
   name: 'app'
 }
+
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
 </style>
--- vuecrud/src/components/Index.vue
+++ vuecrud/src/components/Index.vue
@@ -1,28 +1,22 @@
-// Index.vue
 <template>
   <div>
-    <h1>Phones</h1>
-
-    <table class="table table-hover">
-      <thead>
-        <tr>
-          <td>IMEI</td>
-          <td>Item Name</td>
-          <td>Item Price</td>
-          <td>Actions</td>
-        </tr>
-      </thead>
-
+    <table class="table is-striped is-hoverable">
       <tbody>
+        <tr>
+          <th>IMEI</th>
+          <th>Item Name</th>
+          <th>Item Price</th>
+          <th>Actions</th>
+        </tr>
         <tr v-for="item in items" :key="item.id">
           <td>{{ item.id }}</td>
           <td>{{ item.name }}</td>
           <td>{{ item.price }}</td>
           <td>
-            <router-link :to="{name: 'Edit', params: { id: item.id }}" class="btn btn-primary">Edit</router-link>
+            <router-link :to="{name: 'Edit', params: { id: item.id }}" class="button is-primary is-small">Edit</router-link>
           </td>
           <td>
-            <button class="btn btn-danger" @click="deleteItem(item.id)">Delete</button>
+            <button class="button is-danger is-small" @click="deleteItem(item.id)">Delete</button>
           </td>
         </tr>
       </tbody>
